9b77dc4a6c7011b138c0dc1d8197ff9cd6dfe9c6
MiriaMoreira <msm2@ic.ufal.br>
2025-10-03 23:37:11 -0300

    Hello World com timer

--- src/main.c
+++ src/main.c
@@ -1,10 +1,31 @@
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/device.h>
-#include <zephyr/drivers/pwm.h>
+#include <zephyr/logging/log.h>
 
+LOG_MODULE_REGISTER(hello_timer, LOG_LEVEL_DBG);
+
+#define HELLO_INTERVAL K_MSEC(CONFIG_HELLO_INTERVAL)
+
+static struct k_timer hello_timer;
+
+void timer_handler(struct k_timer *timer_id)
+{
+    static int counter = 0;
+
+    LOG_INF("Hello World! Contador=%d", counter);
+    LOG_DBG("Debug: Timer executou");
+    if (counter % 5 == 0) {
+        LOG_ERR("Erro simulado a cada 5 mensagens!");
+    }
+
+    counter++;
+}
 
 int main(void)
 {
+    LOG_INF("Hello World com Timer...");
+
+    k_timer_init(&hello_timer, timer_handler, NULL);
+    k_timer_start(&hello_timer, HELLO_INTERVAL, HELLO_INTERVAL);
+
     return 0;
 }
