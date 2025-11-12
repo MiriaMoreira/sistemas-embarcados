4d193ad57f5be513407e6045463ebf5e63c7047b
Miriã Moreira <59510633+MiriaMoreira@users.noreply.github.com>
2025-11-12 19:02:36 -0300

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@
 LOG_MODULE_REGISTER(hello_timer, LOG_LEVEL_DBG);
 
 #define HELLO_INTERVAL K_MSEC(CONFIG_HELLO_INTERVAL)
-
 static struct k_timer hello_timer;
 
 void timer_handler(struct k_timer *timer_id)
@@ -12,7 +11,7 @@ void timer_handler(struct k_timer *timer_id)
     static int counter = 0;
 
     LOG_INF("Hello World! Contador=%d", counter);
-    LOG_DBG("Debug: Timer executou");
+    LOG_DBG("Debug: Timer executou.");
     if (counter % 5 == 0) {
         LOG_ERR("Erro simulado a cada 5 mensagens!");
     }
@@ -22,10 +21,15 @@ void timer_handler(struct k_timer *timer_id)
 
 int main(void)
 {
-    LOG_INF("Hello World com Timer...");
+    LOG_INF("Iniciando Hello World com Timer...");
 
     k_timer_init(&hello_timer, timer_handler, NULL);
     k_timer_start(&hello_timer, HELLO_INTERVAL, HELLO_INTERVAL);
 
+    while (1) {
+        k_sleep(K_FOREVER);
+    }
+
     return 0;
 }
+
